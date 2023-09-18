98b36df983d00c4bff6827b25db39fd954085ce9
ita022 <ita022@c7-1.ifi.uit.no>
2023-09-18 13:27:08 +0200

    fixed prev

--- p2p_node.py
+++ p2p_node.py
@@ -9,6 +9,20 @@ def hash_function(key, size):
     return int(hashlib.sha1(key.encode("utf-8")).hexdigest(), 16) % size
 
 
+def get_previous_node(ip, size):
+    with open("ip.txt", "r") as f:
+        entries = [line.strip().split(" - ") for line in f if " - " in line]
+        ips = [entry[1] for entry in entries if len(entry) == 2]
+        nodes = sorted((hash_function(ip, size), ip) for ip in ips)
+
+    for i in range(len(nodes)):
+        if nodes[i][1] == ip:
+            if i == 0:
+                return nodes[len(nodes) - 1][1]
+            else:
+                return nodes[i - 1][1]
+
+
 # Chord node class
 class ChordNode:
     def __init__(self, m=4):
@@ -18,6 +32,9 @@ class ChordNode:
         self.size_of_cluster = 2**m
         self.node_id = hash_function(self.IP, self.size_of_cluster)
         self.key_size = m
+        self.previous = hash_function(
+            get_previous_node(self.IP, self.size_of_cluster), self.size_of_cluster
+        )
 
     def __str__(self):
         return "Node ID: " + str(self.node_id)
@@ -55,5 +72,6 @@ if __name__ == "__main__":
 
     print(str(ChordNode()))
     ChordNode.finger_table(self=node)
+    print("Previous node: " + str(node.previous))
 
     # app.run(host="0.0.0.0", port=8000)
